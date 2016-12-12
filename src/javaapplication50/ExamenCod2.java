309056ab24e47db088594fe1f02ce76d214f04a7
mblancosoto <mblancosoto@victoria24.danielcastelao.org>
2016-12-12 09:51:54 +0100

    Refactorizado,añadidos comentarios finales.

---
+++ src/javaapplication50/ExamenCod2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication50;
+
+/**
+ *
+ * @author mblancosoto
+ */
+public class ExamenCod2 {
+    
+     int j =8;
+      int f,i;
+    
+    //declaramos las variables como variables de clase
+    
+    
+    public void metodoFor(){
+        //creamos el metodoFor para meter aquí el metodo principal
+          if (j == 0)
+f = 1;
+   else
+   {
+f = 1;
+for (i = j; i >= 1; i--)
+{
+    f = f * i;
+}
+        
+        
+        
+    }
+}
+    
+    public void visualizar(){
+        
+        //hago un metodo expresamente para visualizar F
+   System.out.println(f);
+    }
+    
+    
+}
